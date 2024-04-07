bdfa9917453851564a62c16e02d7bcf73fcc804d
Reburen <83378850+Reburen@users.noreply.github.com>
2024-04-07 02:40:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,17 @@ import streamlit as st
 
 st.title('Ejemplo de agregar botones después de hacer clic')
 
-# Variable de estado para controlar si se ha hecho clic en el primer botón
-boton1_clickeado = st.button('Botón 1')
+# Verifica si ya hemos inicializado los botones en la sesión
+if 'botones_inicializados' not in st.session_state:
+    st.session_state.botones_inicializados = False
 
-# Si se hace clic en el Botón 1
-if boton1_clickeado:
+# Si los botones no se han inicializado todavía
+if not st.session_state.botones_inicializados:
+    boton1_clickeado = st.button('Botón 1')
+    st.session_state.botones_inicializados = True
+
+# Si se ha hecho clic en el Botón 1
+if st.session_state.botones_inicializados and boton1_clickeado:
     st.write('Has presionado el Botón 1.')
     # Aquí puedes agregar código adicional que deseas ejecutar después de hacer clic en el Botón 1
     
