f1eb3ffbedb469a7fc1f753e356163ddf2542bf5
Reburen <83378850+Reburen@users.noreply.github.com>
2024-04-07 02:35:08 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,19 @@ button2_clicked = st.button('Botón 2')
 if button1_clicked:
     st.write('Has presionado el Botón 1.')
     # Coloca aquí el código correspondiente al Botón 1
+    button3_clicked = st.button('Botón 3')
+    button4_clicked = st.button('Botón 4')
+    if button3_clicked:
+        st.write('Botón 3')
+    if Button4_clicked:
+        st.write('Botón 4')
 
 if button2_clicked:
     st.write('Has presionado el Botón 2.')
     # Coloca aquí el código correspondiente al Botón 2
+    button3_clicked = st.button('Botón 3')
+    button4_clicked = st.button('Botón 4')
+    if button3_clicked:
+        st.write('Botón 3')
+    if Button4_clicked:
+        st.write('Botón 4')
