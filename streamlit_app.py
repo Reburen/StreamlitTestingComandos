d15f3ff717cfdae097bd56089657bcf1e901b768
Reburen <83378850+Reburen@users.noreply.github.com>
2024-04-07 02:39:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,25 @@
 import streamlit as st
 
-st.title('Opción con dos botones en Streamlit')
+st.title('Ejemplo de agregar botones después de hacer clic')
 
-button1_clicked = st.button('Botón 1')
-button2_clicked = st.button('Botón 2')
+# Variable de estado para controlar si se ha hecho clic en el primer botón
+boton1_clickeado = st.button('Botón 1')
 
-if button1_clicked:
+# Si se hace clic en el Botón 1
+if boton1_clickeado:
     st.write('Has presionado el Botón 1.')
-    # Coloca aquí el código correspondiente al Botón 1
-    button3_clicked = st.button('Botón 3')
-    button4_clicked = st.button('Botón 4')
-    if button3_clicked:
-        st.write('Botón 3')
-    if button4_clicked:
-        st.write('Botón 4')
+    # Aquí puedes agregar código adicional que deseas ejecutar después de hacer clic en el Botón 1
+    
+    # Agregar más botones después de hacer clic en el Botón 1
+    boton2_clickeado = st.button('Botón 2')
+    boton3_clickeado = st.button('Botón 3')
 
-if button2_clicked:
-    st.write('Has presionado el Botón 2.')
-    # Coloca aquí el código correspondiente al Botón 2
-    button3_clicked = st.button('Botón 3')
-    button4_clicked = st.button('Botón 4')
-    if button3_clicked:
-        st.write('Botón 3')
-    if button4_clicked:
-        st.write('Botón 4')
+    # Si se hace clic en el Botón 2
+    if boton2_clickeado:
+        st.write('Has presionado el Botón 2.')
+        # Coloca aquí el código correspondiente al Botón 2
+    
+    # Si se hace clic en el Botón 3
+    if boton3_clickeado:
+        st.write('Has presionado el Botón 3.')
+        # Coloca aquí el código correspondiente al Botón 3
