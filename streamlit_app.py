d89335918d6ce8faeebd45b6724d9c851d59f77e
Reburen <83378850+Reburen@users.noreply.github.com>
2024-04-07 16:34:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,12 @@
 import streamlit as st
 
-# Verifica si los botones se han inicializado en la sesión
-if 'button1_clicked' not in st.session_state:
-    st.session_state.button1_clicked = False
+st.title('Botón de alternancia en Streamlit')
 
-# Primer botón
-button1_clicked = st.button('Botón 1')
+# Definir un checkbox con un valor inicial False
+toggle_button = st.checkbox('Activar/Desactivar')
 
-# Si se hace clic en el Botón 1
-if button1_clicked:
-    st.session_state.button1_clicked = True
-
-# Verifica si los botones se han inicializado en la sesión
-if 'button2_clicked' not in st.session_state:
-    st.session_state.button2_clicked = False
-
-# Segundo botón
-button2_clicked = st.button('Botón 2')
-
-# Si se hace clic en el Botón 2
-if button2_clicked:
-    st.session_state.button2_clicked = True
-
-# Imprimir el estado de los botones
-st.write('Estado del Botón 1:', st.session_state.button1_clicked)
-st.write('Estado del Botón 2:', st.session_state.button2_clicked)
+# Verificar si el checkbox está marcado
+if toggle_button:
+    st.write('El botón de alternancia está activado.')
+else:
+    st.write('El botón de alternancia está desactivado.')
